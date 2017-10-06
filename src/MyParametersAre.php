d5c2cb2d0bb349acdd335d7eadeaa678c7a4f3d0
malukenho <admin@phpse.net>
2017-10-06 14:13:24 +0200

    Use doctrine instantiator to create new default resolvers

--- src/MyParametersAre.php
+++ src/MyParametersAre.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace GuessWhat;
 
+use Doctrine\Instantiator\Instantiator;
+use GuessWhat\ParameterResolver\ParameterResolverInterface;
 use ReflectionMethod;
 use ReflectionParameter;
 
@@ -14,21 +16,39 @@ class MyParametersAre implements GuesserInterface
      */
     private $reflectionMethod;
 
-    public function __construct(ReflectionMethod $reflectionMethod)
-    {
+    /**
+     * @var ParameterResolverInterface[]
+     */
+    private $resolvers;
+
+    public function __construct(
+        ReflectionMethod $reflectionMethod,
+        ?ParameterResolverInterface ...$additionalResolvers
+    ) {
         $this->reflectionMethod = $reflectionMethod;
+        $this->resolvers = array_merge($this->instantiateDefaultResolvers(self::TYPE_RESOLVER), (array) $additionalResolvers);
+    }
+
+    private function instantiateDefaultResolvers(array $defaultResolvers)
+    {
+        $instantiator = new Instantiator();
+
+        return array_map(
+            function (string $resolver) use ($instantiator): ParameterResolverInterface {
+                return $instantiator->instantiate($resolver);
+            },
+            $defaultResolvers
+        );
     }
 
     public function __invoke(): array
     {
         return array_map(
             function (ReflectionParameter $parameter) {
-                foreach (self::TYPE_RESOLVER as $resolver) {
-                    // @todo instantiate the list previously
-                    // @todo probably a chain...
-                    $a = new $resolver;
-                    if ($a->canResolve($parameter)) {
-                        return $a->generateValue();
+                // @todo remote this foreach
+                foreach ($this->resolvers as $resolver) {
+                    if ($resolver->canResolve($parameter)) {
+                        return $resolver->generateValue();
                     }
                 }
             },
